da8543588091436550fe3e79215296524e0fcf7f
Akhil <akhil.nuthakki1@gmail.com>
2023-01-11 16:10:19 +0000

    first commit

---
+++ src/test/java/com/fse2/LmsSignUp/LmsSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fse2.LmsSignUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
